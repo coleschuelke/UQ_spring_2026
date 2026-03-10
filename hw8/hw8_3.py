41d4bdd29ea854189cdac57a1c1d4c91ffcc14de
Cole Schuelke Linux <coleschuelke@gmail.com>
2026-03-09 21:25:35 -0500

    functionality complete

--- hw8/hw8_3.py
+++ hw8/hw8_3.py
@@ -1,5 +1,20 @@
 import numpy as np
 from scipy import stats
+import matplotlib.pyplot as plt
+
+from utils.heat_equation import HeatEquation
+
+eq = HeatEquation(k=2.3e2, Tamb=21.29)
+
+
+# Helper functions
+def Ts(x, q):
+    q_dict = {
+        "phi": q[0],
+        "h": q[1],
+    }
+
+    return eq.Ts_vals(x, q_dict)
 
 
 def draw_lhs(N):
@@ -7,26 +22,19 @@ def draw_lhs(N):
     # Get the bins
     dp = 1 / N
     probs = [n / N for n in range(N + 1)]  # Includes 0, 1
-    # phi_divs = stats.Normal.icdf(probs)  # TODO: Both of these are wrong
-    # h_divs = 0
 
     # Random on the y axis
     ru = np.random.uniform(0, 1, 2 * N)
 
-    phi_bins = []
-    h_bins = []
+    phi = np.zeros((N,))
+    h = np.zeros((N,))
 
     # Draw from the bins
     for i in range(N):
         p_phi = probs[i] + ru[i] * dp
         p_h = probs[i] + ru[N + i] * dp
-        # Phi
-        phi[i] = 0  # TODO: Inverse draw
-        h[i] = 0  # TODO: Inverse draw
-
-        # h
-    phi = []
-    h = []
+        phi[i] = stats.norm.ppf(p_phi, loc=-15e4, scale=np.sqrt(10e8))
+        h[i] = 5 + p_h * 10
 
     # Recombine to realizations
     l = np.array(range(N)).T
@@ -34,20 +42,42 @@ def draw_lhs(N):
     r = rng.permutation(N)
     perms = np.column_stack((l, r.T))
 
-    samples = np.zeros(N, 2)
+    samples = np.zeros((N, 2))
     for i in range(N):
-        samples[i, :] = (phi[perms[i, 0]], h(perms[i, 1]))
+        samples[i, :] = (phi[perms[i, 0]], h[perms[i, 1]])
 
     return samples
 
 
-draw_lhs(5)
 # Brute force monte-carlo
+big = 100_000
+phi_big = np.random.normal(-15e4, np.sqrt(10e8), big)
+h_big = np.random.uniform(5, 15, big)
+q_big = np.column_stack((phi_big, h_big))
+temp_big = Ts(0.38, q_big.T)
+print(q_big.shape)
+mean = np.mean(temp_big)
+var = np.var(temp_big)
+# Quantify the uncertainty on the estimates
+print(f"The mean of the brute force method is: {mean}")
+print(f"The variance of the brute force method is: {var}")
+# Converges at a rate of 1/sqrt(N)
 
 # MC vs LHS head-to-head 100 samples
+n_samp = 100
+phi_samp = np.random.normal(-15e4, np.sqrt(10e8), n_samp)
+h_samp = np.random.uniform(5, 15, n_samp)
+mc_samples = np.column_stack((phi_samp, h_samp))
+lhs_samples = draw_lhs(n_samp)
 
 
 #### Plotting ####
 # 2D plots
+plt.scatter(lhs_samples[:, 0], lhs_samples[:, 1], label="LHS")
+plt.scatter(mc_samples[:, 0], mc_samples[:, 1], label="MC")
+plt.legend()
 
 # Histogram H2H samples
+
+
+plt.show()
