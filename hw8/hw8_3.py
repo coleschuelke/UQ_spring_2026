72ebf25c5a889f9abba455e230d06e22544bf633
Cole Schuelke Linux <coleschuelke@gmail.com>
2026-03-10 23:49:11 -0500

    finish 3

--- hw8/hw8_3.py
+++ hw8/hw8_3.py
@@ -37,16 +37,11 @@ def draw_lhs(N):
         h[i] = 5 + p_h * 10
 
     # Recombine to realizations
-    l = np.array(range(N)).T
     rng = np.random.default_rng()
-    r = rng.permutation(N)
-    perms = np.column_stack((l, r.T))
+    rng.shuffle(phi)
+    rng.shuffle(h)
 
-    samples = np.zeros((N, 2))
-    for i in range(N):
-        samples[i, :] = (phi[perms[i, 0]], h[perms[i, 1]])
-
-    return samples
+    return np.column_stack((phi, h))
 
 
 # Brute force monte-carlo
@@ -59,8 +54,9 @@ print(q_big.shape)
 mean = np.mean(temp_big)
 var = np.var(temp_big)
 # Quantify the uncertainty on the estimates
-print(f"The mean of the brute force method is: {mean}")
-print(f"The variance of the brute force method is: {var}")
+# print(f"The mean of the brute force method is: {mean}")
+# print(f"The variance of the brute force method is: {var}")
+print(f"The mean and variance using brute force: E[Y] = {mean}, Var(Y) = {var}")
 # Converges at a rate of 1/sqrt(N)
 
 # MC vs LHS head-to-head 100 samples
@@ -70,14 +66,50 @@ h_samp = np.random.uniform(5, 15, n_samp)
 mc_samples = np.column_stack((phi_samp, h_samp))
 lhs_samples = draw_lhs(n_samp)
 
+# Push the samples through the function
+lhs_temps = Ts(0.38, lhs_samples.T)
+mc_temps = Ts(0.38, mc_samples.T)
+
+lhs_mean = np.mean(lhs_temps)
+lhs_var = np.var(lhs_temps)
+mc_mean = np.mean(mc_temps)
+mc_var = np.var(mc_temps)
+print(f"The mean and variance using LHS: E[Y] = {lhs_mean}, Var(Y) = {lhs_var}")
+print(f"The mean and variance using MC: E[Y] = {mc_mean}, Var(Y) = {mc_var}")
+
 
 #### Plotting ####
 # 2D plots
-plt.scatter(lhs_samples[:, 0], lhs_samples[:, 1], label="LHS")
-plt.scatter(mc_samples[:, 0], mc_samples[:, 1], label="MC")
-plt.legend()
+fig, ax = plt.subplots()
+ax.scatter(lhs_samples[:, 0], lhs_samples[:, 1])
+ax.set_ylim([5, 15])
+ax.set_xlim([-210000, -65000])
+ax.set_xlabel(r"$\Phi$")
+ax.set_ylabel(r"h")
+ax.set_title("Latin Hypercube Sampling")
+fig, ax = plt.subplots()
+ax.scatter(mc_samples[:, 0], mc_samples[:, 1])
+ax.set_ylim([5, 15])
+ax.set_xlim([-210000, -65000])
+ax.set_xlabel(r"$\Phi$")
+ax.set_ylabel(r"h")
+ax.set_title("Monte-Carlo")
 
-# Histogram H2H samples
+# Histograms
+fig, axes = plt.subplots(2, 1)
+axes[0].hist(lhs_samples[:, 0], density=True, bins=20)
+axes[0].set_xlabel(r"$\Phi$")
+axes[1].hist(lhs_samples[:, 1], density=True, bins=20)
+axes[1].set_xlabel("h")
+fig.suptitle("LHS Histograms")
+plt.tight_layout()
 
+fig, axes = plt.subplots(2, 1)
+axes[0].hist(mc_samples[:, 0], density=True, bins=20)
+axes[0].set_xlabel(r"$\Phi$")
+axes[1].hist(mc_samples[:, 1], density=True, bins=20)
+axes[1].set_xlabel("h")
+fig.suptitle("Monte-Carlo Histograms")
 
+plt.tight_layout()
 plt.show()
